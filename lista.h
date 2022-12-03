41dfeea4169de372547f0e92b9910f4dd3813637
Déborah Andrade <104578483+deborah-andrade@users.noreply.github.com>
2022-12-03 20:45:38 -0300

    Create lista.h

---
+++ lista.h
@@ -0,0 +1,14 @@
+#define MAX 5
+
+typedef struct lista Lista;
+
+Lista *criar();
+
+void append(Lista *l, int valor);
+int get(Lista *l, int index); 
+int remover(Lista *l, int index);
+void insert_sorted(Lista *l, int valor); 
+void insert(Lista *l, int valor);
+int full(Lista *l);
+int empty(Lista *l);
+void print(Lista *l);
