93251d585c88a158a7332025736def4c492630d5
Déborah Andrade <104578483+deborah-andrade@users.noreply.github.com>
2022-12-03 20:44:50 -0300

    Create lista.c

---
+++ lista.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+
+
+struct lista {
+    int quant;
+    int vet[MAX];
+};
+
+
+Lista *criar() {
+    Lista *l;
+    l = (Lista *) malloc(sizeof(Lista));
+    if (l != NULL) {
+        l->quant = 0;
+    }
+
+    return l;
+}
+
+void append(Lista *l, int valor) {
+    l->vet[l->quant] = valor;
+    l->quant++;
+}
+
+int get(Lista *l, int index) {
+    return l->vet[index];
+}
+
+int remover(Lista *l, int index){
+    for (int i = 0; i < l->quant; i++){
+        for (int i = index; i < l->quant - 1; i++) {
+        l->vet[i] = l->vet[i + 1];
+    }
+    l->quant--;
+    }
+}
+
+void insert(Lista *l, int valor) {
+    for (int i = l->quant - 1; i >= 0; i--) {
+        l->vet[i + 1] = l->vet[i];
+    }
+    l->vet[0] = valor;
+    l->quant++;
+
+}
+
+void insert_sort(Lista *l, int valor) {
+    int i, j, x;
+
+    for (i = l->quant - 1; i >= 0; i--) {
+        l->vet[i + 1] = l->vet[i];
+    }
+    l->vet[0] = valor;
+    l->quant++;
+
+    for(i = 1; i < valor; i++) {
+    x = l->vet[i];
+    j = i - 1;
+    while(j >= 0 && l->vet[j] > x) {
+        l->vet[j+1] = l->vet[j];
+        j--;
+        }
+        l->vet[j+1] = x;
+    }
+}
+
+int full(Lista *l){
+    if(l->quant == MAX){
+        printf("\nA lista esta cheia!");
+    }
+}
+
+
+int empty(Lista *l){
+    if (l->quant == 0){
+        printf("\nA lista esta vazia!");
+    }
+        
+}
+
+
+void print(Lista *l){
+        for (int i = 0; i < l->quant; i++) {
+        printf("%d ", l->vet[i]);
+    }
+}
+
+
+
+int main(){
+    Lista *lista;
+    lista = criar();
+    append(lista, 1);
+    append(lista, 2);
+    insert(lista, 3);
+    insert(lista, 4);
+    printf("A lista estando de forma ordenada: ");
+    insert_sort(lista, 5);
+    print(lista);
+    printf("\n");
+    remover(lista, 4);
+    print(lista);
+    printf("\nO valor do indice que deseja encontrar e: %d\n", get(lista, 2));
+    full(lista);
+
+}
