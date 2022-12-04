6aa7b78c230e98dd150b37879e10060b01d1bf7d
Déborah Andrade <104578483+deborah-andrade@users.noreply.github.com>
2022-12-04 15:56:28 -0300

    Update lista.h

--- lista.h
+++ lista.h
@@ -6,9 +6,10 @@ Lista *criar();
 
 void append(Lista *l, int valor);
 int get(Lista *l, int index); 
-int remover(Lista *l, int index);
+void remover(Lista *l, int index);
 void insert_sorted(Lista *l, int valor); 
 void insert(Lista *l, int valor);
 int full(Lista *l);
 int empty(Lista *l);
+int size(Lista *l);
 void print(Lista *l);
